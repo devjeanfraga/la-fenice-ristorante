bdb7449442afa7d53dc547298869017cbe124809
Jean Fraga <jeandevjs@gmail.com>
2023-05-22 23:19:03 -0300

    Add preloader

---
+++ src/components/preload/Preload.tsx
@@ -0,0 +1,10 @@
+import './Preload.css';
+
+export const Preload = () => {
+  return (
+    <div className="preload" id="preload" >
+      <div className="circle"></div>
+        <p className="text">La Fenice</p>
+    </div>
+  );
+};
