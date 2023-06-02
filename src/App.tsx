c524569d70bb9a1cebf6f17741294e6e5891ce4b
Jean Fraga <jeandevjs@gmail.com>
2023-06-02 11:26:51 -0300

    update assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Preload } from './components/preload/Preload';
+import { Topbar} from './components/topbar/Topbar';
+import { Header } from './components/header/Header';
+import { Hero } from './components/hero/Hero';
 import './App.css';
 
 function App() {
+  const [ loading, setLoading ] = useState(true);
+  
+  useEffect(()=> {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* { loading && <Preload/> } */}
+      <Topbar/>
+      <Header/>
+      <Hero/>
     </div>
   );
 }
