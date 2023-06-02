66505f90be8c3d499ac5ba45fd7c3d873afa4bc4
Jean Fraga <jeandevjs@gmail.com>
2023-06-02 11:31:36 -0300

    feat: add button

---
+++ src/components/buttom/Button.tsx
@@ -0,0 +1,11 @@
+import './Button.css'
+export const Button = () => {
+  return (
+    <div className='btn'>
+      <a>
+        <span className="text text-1">Visualizza il nostro menu </span>
+        <span className="text text-2">Visualizza il nostro menu </span>
+      </a>
+    </div>
+  )
+}
