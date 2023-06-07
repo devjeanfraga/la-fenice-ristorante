1aa8507a35dce736935e930948ab12cc638488ed
Jean Fraga <jeandevjs@gmail.com>
2023-06-06 22:45:46 -0300

    move buttom to a single component

--- src/components/buttom/Button.tsx
+++ src/components/buttom/Button.tsx
@@ -3,8 +3,8 @@ export const Button = () => {
   return (
     <div className='btn'>
       <a>
-        <span className="text text-1">Visualizza il nostro menu </span>
-        <span className="text text-2">Visualizza il nostro menu </span>
+        <span className="text text-1">Visualizza il nostro menu</span>
+        <span className="text text-2">Visualizza il nostro menu</span>
       </a>
     </div>
   )
