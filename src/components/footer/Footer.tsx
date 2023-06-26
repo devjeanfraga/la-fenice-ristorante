a0b4c8368339fd46564a101c6d5c340d0a76f7aa
Jean Fraga <jeandevjs@gmail.com>
2023-06-26 17:22:35 -0300

    Add footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+
+export const Footer =  () => {
+  return (
+    <footer className="footer">
+      <div>
+        <h3>Copyright © 2023. Tutti i diritti riservati</h3>
+      </div>
+    </footer>
+  );
+};
