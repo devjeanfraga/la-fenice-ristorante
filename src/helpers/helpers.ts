0a3526e0938fc6b1b4d490371e360509d28c33f1
Jean Fraga <jeandevjs@gmail.com>
2023-07-03 13:57:32 -0300

    refactor

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -3,17 +3,18 @@
  * Add and Remove event listener on multiply elements
  */
 
-export const addEventOnElements = (elements: NodeListOf<any>, eventType: string, callback: any) => {
+export const addEventOnElements = (elements: NodeListOf<any> | HTMLCollection, eventType: string, callback: any) => {
   for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
 
-export const removeEventOnElements = (elements: NodeListOf<any>, eventType: string, callback: any) => {
+export const removeEventOnElements = (elements: NodeListOf<any> | HTMLCollection, eventType: string, callback: any) => {
   for (let i = 0; i < elements.length; i++) {
     elements[i].removeEventListener(eventType, callback);
   }
 }
+
 
 
 
--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -3,6 +3,6 @@ import React, {useState} from 'react';
 export default (initialValue:boolean = false) => {
   const [ value, setValue] = useState(initialValue);
 
-  const handleValue = () => setValue( initialValue => !initialValue );
+  const handleValue = ():void => setValue( initialValue => !initialValue );
   return [ value , handleValue] as const; 
 }
