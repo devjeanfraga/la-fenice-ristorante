b81b7f2a1fca88e750d37f2777390db14c9f82e4
Jean Fraga <jeandevjs@gmail.com>
2023-07-09 18:16:19 -0300

    move menu data to a single file

---
+++ src/data/menu-data.ts
@@ -0,0 +1,197 @@
+import {
+  flanDiZucca,
+  polenta,
+  tagliereRustico,
+  tartareDiManzo
+} from '../assets/images/menu-items'
+
+import {
+  bigoli,
+  cappellacci,
+  gnocchetti,
+  pappardelle,
+  ravioli,
+  risottoPorcini,
+  tagliolino
+} from '../assets/images/menu-items'
+
+
+import {
+  tagliataDiManzo,
+  cotolettaAllaMilanese,
+  rosticciataDiMaialino,
+  filetoDiManzo,
+  filetoDiTonno,
+  stincoBrasato,
+  castratoAllaBrace,
+  baccala,
+  somarinoConPolenta,
+  galletoAllaDiavola,
+  guanciottoDiManzo
+} from '../assets/images/menu-items'
+
+export const data =  {
+  antipasti: [
+    {
+      foodName: 'Flan di zucca',
+      price: '8,00 €',
+      img: flanDiZucca,
+      description: 'Deliziosi antipasti autunnali, piccole monoporzioni dal sapore delicato, ideali come aperitivo sfizioso. In contrasto con il gusto dolce della polpa di zucca, abbiamo guarnito i flan con una crema al Gorgonzola, un formaggio erborinato dal sapore deciso.'
+    },
+    {
+      foodName: 'Polenta gratinata con funghi e formaggi',
+      price: '8,00 €',
+      img: polenta,
+      description: 'Piatto rustico, semplicissimo e molto saporito, perfetto per il giorni freddi e piovosi… una gustosa polenta gratinata con funghi e formaggio filante, facile e veloce, ideale anche come piatto unico.'
+    },
+    {
+      foodName: 'Tagliere rustico di salumi e formaggi',
+      price: '12,00 €',
+      img: tagliereRustico,
+      description: 'Un piatto semplice, almeno in apparenza, apprezzato da tutti perché ognuno può scegliere anche solo uno dei prodotti offerti',
+    },
+    {
+      foodName: 'Tartare di manzo',
+      price: '14,00 €',
+      img: tartareDiManzo,
+      description: 'Un piatto fresco profumato e proteico, personalizzato con aromi e verdure croccanti.',
+    },
+    {
+      foodName: 'Flan di zucca',
+      price: '8,00 €',
+      img: flanDiZucca,
+      description: 'Deliziosi antipasti autunnali, piccole monoporzioni dal sapore delicato, ideali come aperitivo sfizioso. In contrasto con il gusto dolce della polpa di zucca, abbiamo guarnito i flan con una crema al Gorgonzola, un formaggio erborinato dal sapore deciso.'
+    },
+    {
+      foodName: 'Polenta gratinata con funghi e formaggi',
+      price: '8,00 €',
+      img: polenta,
+      description: 'Piatto rustico, semplicissimo e molto saporito, perfetto per il giorni freddi e piovosi… una gustosa polenta gratinata con funghi e formaggio filante, facile e veloce, ideale anche come piatto unico.'
+    },
+    {
+      foodName: 'Tagliere rustico di salumi e formaggi',
+      price: '12,00 €',
+      img: tagliereRustico,
+      description: 'Un piatto semplice, almeno in apparenza, apprezzato da tutti perché ognuno può scegliere anche solo uno dei prodotti offerti',
+    },
+    {
+      foodName: 'Tartare di manzo',
+      price: '14,00 €',
+      img: tartareDiManzo,
+      description: 'Un piatto fresco profumato e proteico, personalizzato con aromi e verdure croccanti.',
+    }
+  ],
+  primi: [
+    {
+      foodName: 'Bigoli con salsiccia broccoli e pecorino',
+      price: '12,00 €',
+      img: bigoli,
+      description: ''
+    },
+    {
+      foodName: 'Cappellacci di zucca tartufati',
+      price: '12,00 €',
+      img: cappellacci,
+      description: 'In Alternativa anche con Ragù o Burro E Salvia'
+    },
+    {
+      foodName: 'Gnocchetti gorgorzola e radicchio',
+      price: '12,00 €',
+      img: gnocchetti,
+      description: ''
+    },
+    {
+      foodName: 'Pappardelle alla montanara',
+      price: '12,00 €',
+      img: pappardelle,
+      description: 'Salsa Con Pomodoro Speck Stufato e Funghi Porcini'
+    },
+    {
+      foodName: 'Ravioli ai porcini con speck sascarpone e noci',
+      price: '12,00 €',
+      img: ravioli,
+      description: ''
+    },
+    {
+      foodName: 'Risotto porcini e taleggio',
+      price: '14,00 €',
+      img: risottoPorcini,
+      description: ''
+    },
+    {
+      foodName: 'Tagliolino rustico alla carrettiera',
+      price: '14,00 €',
+      img: tagliolino,
+      description: 'Serviti su un letto di rucola con pomodorini basilico e mozzarella all uscita'
+    },
+  ],
+  secondi: [
+    {
+      foodName: 'Tagliata di manzo alla toscana con patate',
+      price: '20,00 €',
+      img: tagliataDiManzo,
+      description: ''
+    },
+    {
+      foodName: 'Cotoletta alla milanese',
+      price: '20,00 €',
+      img: cotolettaAllaMilanese,
+      description: ''
+    },
+    {
+      foodName: 'Rosticciata di maialino con patate',
+      price: '18,00 €',
+      img: rosticciataDiMaialino,
+      description: ''
+    },
+    {
+      foodName: 'Filetto di manzo alla brace',
+      price: '20,00 €',
+      img: filetoDiManzo,
+      description: ''
+    },
+    {
+      foodName: 'Filetto di tonno alla catalana',
+      price: '20,00 €',
+      img: filetoDiTonno,
+      description: 'Servito Con Misticanza, Cipolla Rossa E Crema Di Aceto Balsamico'
+    },
+    {
+      foodName: 'Stinco brasato con patate',
+      price: '18,00 €',
+      img: stincoBrasato,
+      description: ''
+    },
+    {
+      foodName: 'Castrato alla brace',
+      price: '18,00 €',
+      img: castratoAllaBrace,
+      description: ''
+    },
+    {
+      foodName: 'Baccala alla vicentina con polenta',
+      price: '18,00 €',
+      img: baccala,
+      description: ''
+    },
+    {
+      foodName: 'Somarino con polenta',
+      price: '18,00 €',
+      img: somarinoConPolenta,
+      description: ''
+    },
+    {
+      foodName: 'Galletto alla Diavola',
+      price: '15,00 €',
+      img: galletoAllaDiavola,
+      description: ''
+    },
+    {
+      foodName: 'Guanciotto di manzo brasato al vino rosso',
+      price: '15,00 €',
+      img: guanciottoDiManzo,
+      description: ''
+    },
+  ]
+};
+
